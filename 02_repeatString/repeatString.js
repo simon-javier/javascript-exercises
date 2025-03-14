e5352ecc2f565f8ece4f67783ea0b9656cb2d110
Simon <168045868+simon-javier@users.noreply.github.com>
2025-03-14 20:23:36 +0800

    finished 01 and 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(text, count) {
+  let result = "";
+  if (count < 0) return "ERROR";
 
+  for (let i = 0; i < count; i++) {
+    result += text;
+  }
+  return result;
 };
 
 // Do not edit below this line
