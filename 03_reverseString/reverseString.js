b53705af1dfe57f381902b804671671054bccb9b
Simon <168045868+simon-javier@users.noreply.github.com>
2025-03-14 21:00:18 +0800

    finished 03 and 04 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let arr = [];
+  for (let i = 0; i < string.length; i++) {
+    arr.unshift(string.charAt(i));
+  }
+  return arr.join("");
 };
+
+/* My solution seems to be slower than the actual solution because my lack of
+   knowledge in javascript. I should work more on mastering this language.*/
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbersToRemove) {
+  const result = arr.filter((el) => !numbersToRemove.includes(el));
+  return result;
 };
+
+/* I can just return it directly without declaring a variable */
 
 // Do not edit below this line
 module.exports = removeFromArray;
