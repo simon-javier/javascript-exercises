2b8a8c41f8f8bd61ac895a893478edab71bdef6d
Simon <168045868+simon-javier@users.noreply.github.com>
2025-03-19 10:23:34 +0800

    finished 06 exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 !== 0) {
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
+
+// When comparing my solution to the actual solution. The actual solution
+// declare a variable to make the code more readable. My solution has nested if
+// while the actual solution used logical operators more to avoide nested if.
+
 
 // Do not edit below this line
 module.exports = leapYears;
