07e2dff82fa439f4e1914caf6478541ecea77599
Simon <168045868+simon-javier@users.noreply.github.com>
2025-03-19 18:57:06 +0800

    finished 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+  let celsius = (val - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(val) {
+  let fahrenheit = val * (9 / 5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
+
+convertToCelsius(100);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// I actually prefer my code because it is more readable although it is slower
+// than the actual solution because of String conversion to Number.
